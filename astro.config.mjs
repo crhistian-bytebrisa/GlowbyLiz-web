0e1b1828a51f02b15b81f50a44a46bbcfc1c8c67
J.Crhistian <j.crhistiantr@gmail.com>
2026-08-16 17:22:32 -0400

    Deploy 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://crhistian-bytebrisa.github.io",
-    base: '/glowbyliz',
+  site: 'https://TU-USUARIO.github.io',
+  base: '/GlowbyLiz-web/',
 });
